61dce2fd2a779dc8a5b3105acadbf47cdfa6fb36
byakuya2611 <hunganhhp123456@gmail.com>
2021-06-15 12:14:38 +0700

    update

---
+++ adf1/15.06.21/src/main/java/com/product/Product.java
@@ -0,0 +1,57 @@
+package com.product;
+
+import java.util.Scanner;
+
+public abstract class Product {
+    protected String proId;
+    protected String proName;
+    protected int year;
+    protected float price;
+    protected Scanner getScanner() {
+        return new Scanner(System.in);
+    }
+
+    public Product() {}
+
+    public Product(String proId, String proName, int year, float price) {
+        this.proId = proId;
+        this.proName = proName;
+        this.year = year;
+        this.price = price;
+    }
+
+    public abstract void input();
+    public abstract void display();
+
+    public String getProId() {
+        return proId;
+    }
+
+    public void setProId(String proId) {
+        this.proId = proId;
+    }
+
+    public String getProName() {
+        return proName;
+    }
+
+    public void setProName(String proName) {
+        this.proName = proName;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+}
---
+++ adf1/15.06.21/src/main/java/com/product/computers/Computer.java
@@ -0,0 +1,59 @@
+package com.product.computers;
+
+import com.product.Product;
+
+public class Computer extends Product {
+    private String speed;
+    private String producer;
+    public Computer() {}
+
+    public Computer(String proId, String proName, int year, float price,
+                    String speed, String producer) {
+        super(proId, proName, year, price);
+        this.speed = speed;
+        this.producer = producer;
+    }
+
+    @Override
+    public void input() {
+        System.out.println("Enter Computer's id: ");
+        this.proId = getScanner().next();
+
+        System.out.println("Enter Computer's name: ");
+        this.proName = getScanner().next();
+
+        System.out.println("Enter Computer's year: ");
+        this.year = getScanner().nextInt();
+
+        System.out.println("Enter Computer's price: ");
+        this.price = getScanner().nextFloat();
+
+        System.out.println("Enter Computer's speed: ");
+        this.speed = getScanner().next();
+
+        System.out.println("Enter Computer's producer: ");
+        this.producer = getScanner().next();
+    }
+
+    @Override
+    public void display() {
+        System.out.println("id: " + proId + "name: " + proName + "year: " + year +
+                            "price: " + price + "speed: " + speed + "producer: " + producer);
+    }
+
+    public String getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(String speed) {
+        this.speed = speed;
+    }
+
+    public String getProducer() {
+        return producer;
+    }
+
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
+}
